86cb27c899ffde4b2c64dddc2243ae3868baf70d
Raizun <132305024+Raizunx@users.noreply.github.com>
2023-05-04 21:15:36 +0300

    DonationProject.java

--- DonationProject.java
+++ DonationProject.java
@@ -1,5 +1,6 @@
 package donationproject;
 
+import java.io.IOException;
 import java.util.*;
 
 /**
@@ -9,23 +10,18 @@ import java.util.*;
 public class DonationProject {
      
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         Scanner input = new Scanner(System.in); 
         System.out.println("welcome in life in Donation app\n");
-        System.out.print("""
-                         1- Donor 
-                         2- Those in need 
-                         please enter the number that represent you: """);
+        System.out.print("1- Donor \n2- Those in need \nplease enter the number that represent you: ");
         int UserType = input.nextInt();
         
         //check input
         while(UserType != 1 && UserType != 2){
-            System.out.print("""
-                             
-                             !! wrong input !!
-                             1- Donor 
-                             2- Those in need
-                             please enter the number that represent you: """);
+            System.out.print("\n!! wrong input !!"
+                    + "\n1- Donor "
+                    + "\n2- Those in need "
+                    + "\nplease enter the number that represent you: ");
             UserType = input.nextInt();
         }
         
@@ -39,21 +35,17 @@ public class DonationProject {
     
     public static void donor(){
         Scanner input = new Scanner(System.in); 
-        System.out.println("""
-                              
-                           1- List of need
-                           2- Display what to donate
-                           Please enter the number of what you want: """);
+        System.out.print("\n1- List of need "
+                + "\n2- Display what to donate "
+                + "\nPlease enter the number of what you want: ");
         int userSelection = input.nextInt();
         
         //check input
          while(userSelection != 1 && userSelection != 2){
-            System.out.print("""
-                             
-                             !! wrong input !!
-                             1- List of need
-                             2- Display what to donate
-                             Please enter the number of what you want: """);
+            System.out.print("\n!! wrong input !! "
+                    + "\n1- List of need "
+                    + "\n2- Display what to donate "
+                    + "\nPlease enter the number of what you want: ");
             userSelection = input.nextInt();
         }
          
@@ -67,50 +59,52 @@ public class DonationProject {
         String name, status, type;
         int duration = 0;
         
-        System.out.println("Enter the name of the device: ");
+        System.out.print("\nEnter the name of the device: ");
         name = input.nextLine();
         
-        System.out.println("used or new: ");
+        System.out.print("used or new: ");
         status = input.nextLine();
         
         if(status.equalsIgnoreCase("used")){
-            System.out.println("Duration of use:");
+            System.out.print("Device usage time in years: ");
             duration = input.nextInt();
+            String line;
+            line = input.nextLine();
         }
         
-        System.out.println("electronic or normal:");
+        System.out.print("electronic or normal: ");
         type = input.next();
         
         device addDevice = new device(name,status,type,duration);
         try{
         addDevice.addDevice();
-        System.out.println("""
-                           device successfully added.
-                            We will contact you to set a date for receiving the device""");
+        System.out.println("\ndevice successfully added. "
+                + "\nWe will contact you to set a date for receiving the device");
         }catch(Exception e){
-            System.out.println("There was an error. Try later");
+            System.out.println("\nThere was an error. Try later");
         }
     }
     
-    public static void thoseInNeed(){
+    public static void thoseInNeed() throws IOException{
+        
         Scanner input = new Scanner(System.in); 
-        System.out.println("""
-                           1- List of available medical devices.
-                           2- Add a specific need. 
-                           Please enter the number of what you want: """);
+        System.out.print("\n1- List of available medical devices. "
+                + "\n2- Add a specific need. "
+                + "\nPlease enter the number of what you want: ");
         int userSelection = input.nextInt();
         
         //check input
          while(userSelection != 1 && userSelection != 2){
-            System.out.print("""
-                             
-                             !! wrong input !!
-                             1- List of available medical devices.
-                             2- Add a specific need. 
-                             Please enter the number of what you want: """);
+            System.out.print("\n!! wrong input !! "
+                    + "\n1- List of available medical devices. "
+                    + "\n2- Add a specific need. "
+                    + "\nPlease enter the number of what you want: ");
             userSelection = input.nextInt();
          }
-         
+         if(userSelection == 1){
+            list_of_devices list=new list_of_devices();
+            list.available_devices_list();
+         }
          
     }
     
